3bfaa45f01546432e9980031801d497ea5a55496
pizzosta <pizzostor@gmail.com>
2023-07-29 15:30:57 +0100

    Pasclas Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    if n == 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(2, n + 1):
+        row = [None] * i
+        row[0], row[-1] = 1, 1
+
+        for j in range(1, i - 1):
+            row[j] = triangle[-1][j - 1] + triangle[-1][j]
+
+        triangle.append(row)
+
+    return triangle
