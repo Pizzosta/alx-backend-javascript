9ed651d0c41e656f3be10cebad6f44832af41126
pizzosta <pizzostor@gmail.com>
2023-10-11 00:49:34 +0100

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,30 +1,13 @@
 const request = require('request');
 const {expect} = require('chai');
 
-describe('API Tests', () => {
-  let server;
-
-  before(() => {
-    server = require('./api');
-  });
-
-  after(() => {
-    server.close();
-  });
-
-  describe('Index Page', () => {
-    it('should return status code 200', (done) => {
-      request('http://localhost:7865', (error, response) => {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-
-    it('should return the message "Welcome to the payment system"', (done) => {
-      request('http://localhost:7865', (error, response, body) => {
-        expect(body).to.equal('Welcome to the payment system');
-        done();
-      });
+describe('API Integration test', () => {
+  const URL = 'http://localhost:7865';
+  it('GET / should return correct response', (done) => {
+    request.get(`${URL}`, (_, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
   });
 });
